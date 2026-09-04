29c637339c8702c995dd7f096594ec7a26bf56d2
Daniel Dada <tur36985@temple.edu>
2026-09-04 12:45:23 -0400

    Add 2 dollar transaction fee for withdrawls

--- BankAccount.java
+++ BankAccount.java
@@ -9,10 +9,10 @@ public class BankAccount {
         // TODO: add the amount to the balance
     }
     public boolean withdraw(double amount) {
-        // TODO:
-        // Withdraw the requested amount if sufficient funds are available.
-        // Return true if the withdrawal succeeds.
-        // Return false otherwise. return false;
+        if (balance >= amount + 2) {
+            balance -= (amount + 2);
+            return true;
+        }
         return false;
     }
     public String getAccountSummary() {
