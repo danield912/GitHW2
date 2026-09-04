9c863581ae2f52d49f477e5e446ed1fe110fcd6f
Daniel Dada <tur36985@temple.edu>
2026-09-04 12:44:11 -0400

    add mini 50 balance requirement for withdrawls

--- BankAccount.java
+++ BankAccount.java
@@ -9,10 +9,10 @@ public class BankAccount {
         // TODO: add the amount to the balance
     }
     public boolean withdraw(double amount) {
-        // TODO:
-        // Withdraw the requested amount if sufficient funds are available.
-        // Return true if the withdrawal succeeds.
-        // Return false otherwise. return false;
+        if (balance - amount >= 50) {
+            balance -= amount;
+            return true;
+        }
         return false;
     }
     public String getAccountSummary() {
